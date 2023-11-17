131b27a442e979aeee0973b8399d629bb78c212f
Pascual Cano <pascual@allhuman.com>
2023-11-17 09:48:25 +0100

    adding onClick handlers

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,6 @@ const setupSlider = slider => {
 	const container = slider.querySelector(".slider__container");
 	const prev = slider.querySelector(".slider__prev");
 	const next = slider.querySelector(".slider__next");
-	prev.disabled = true;
 
 	const cards = Array.from(slider.querySelectorAll("li"));
 	const cardsLeftOffset = cards.map(card => card.offsetLeft);
@@ -15,13 +14,21 @@ const setupSlider = slider => {
 	const onScroll = () => {
 		const maxScroll = container.scrollWidth - container.offsetWidth;
 		const scrollLeft = container.scrollLeft;
-		prev.disabled = scrollLeft <= 50;
-		next.disabled = scrollLeft >= maxScroll - 50;
-		prevCard = cardsRightOffset.findIndex(offset => offset >= scrollLeft);
+		prev.disabled = scrollLeft <= 0;
+		next.disabled = scrollLeft >= maxScroll;
+		prevCard = cardsLeftOffset.findIndex(offset => offset >= scrollLeft) - 1;
 		nextCard = cardsRightOffset.findIndex(offset => offset > scrollLeft + container.offsetWidth);
 	};
-
 	container.addEventListener("scroll", onScroll);
+	onScroll();
+
+	// on buttons click
+	const onNextClick = () => container.scrollTo({ left: cardsLeftOffset[nextCard], behavior: "smooth" });
+	next.addEventListener("click", onNextClick);
+	const onPrevClick = () => {
+		container.scrollTo({ left: cardsRightOffset[prevCard] - container.offsetWidth, behavior: "smooth" });
+	};
+	prev.addEventListener("click", onPrevClick);
 };
 
 const setup = () => {
