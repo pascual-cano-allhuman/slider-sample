29ffe138eefb39bac7623cfd65e104b66284a58d
Pascual Cano <pascual@allhuman.com>
2023-11-17 08:28:47 +0100

    Component setup

--- src/js/app.js
+++ src/js/app.js
@@ -2,5 +2,10 @@ import "../scss/app.scss";
 
 /* Your JS Code goes here */
 
-/* Demo JS */
-import "./demo.js";
+const setup = () => {
+	const slider = document.querySelector(".slider");
+	const sliderPrev = document.querySelector(".slider__next");
+	const sliderNext = document.querySelector(".slider__next");
+};
+
+setup();
