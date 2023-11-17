1f8f178a1a34166d7bb3fa5fc76ff9ccb40e0152
Pascual Cano <pascual@allhuman.com>
2023-11-17 09:12:17 +0100

    introducing onScroll handler

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,32 @@
 import "../scss/app.scss";
 
-/* Your JS Code goes here */
+const setupSlider = slider => {
+	const container = slider.querySelector(".slider__container");
+	const prev = slider.querySelector(".slider__prev");
+	const next = slider.querySelector(".slider__next");
+	prev.disabled = true;
+
+	const cards = Array.from(slider.querySelectorAll("li"));
+	const cardsLeftOffset = cards.map(card => card.offsetLeft);
+	const cardsRightOffset = cardsLeftOffset.map(offset => offset + cards[0].offsetWidth);
+	let prevCard, nextCard;
+
+	// on scroll calculations
+	const onScroll = () => {
+		const maxScroll = container.scrollWidth - container.offsetWidth;
+		const scrollLeft = container.scrollLeft;
+		prev.disabled = scrollLeft <= 50;
+		next.disabled = scrollLeft >= maxScroll - 50;
+		prevCard = cardsRightOffset.findIndex(offset => offset >= scrollLeft);
+		nextCard = cardsRightOffset.findIndex(offset => offset > scrollLeft + container.offsetWidth);
+	};
+
+	container.addEventListener("scroll", onScroll);
+};
 
 const setup = () => {
-	const slider = document.querySelector(".slider");
-	const sliderPrev = document.querySelector(".slider__next");
-	const sliderNext = document.querySelector(".slider__next");
+	const slider = document.querySelector("#slider");
+	setupSlider(slider);
 };
 
 setup();
